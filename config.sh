de1e32053388ad821e34d888bfe94cf686e57864
Murilo Belluzzo <murilo.belluzzo@intel.com>
2019-05-20 14:04:50 -0700

    Use more meaningful names for variables and ...
    ... remove explict references to "downstream" since the code will be
    leveraged to maintain 'headless forks' as well (i.e. no upstream).
    
    Signed-off-by: Murilo Belluzzo <murilo.belluzzo@intel.com>

--- config.sh
+++ config.sh
@@ -1,23 +1,37 @@
 #! /usr/bin/env bash
-# Copyright (c) 2018 Intel Corporation
+# Copyright (c) 2019 Intel Corporation
 #
 # SPDX-License-Identifier: Apache-2.0
 
-DSTREAM_NAME="" # Name for this Downstream of Clear Linux
+# ==============================================================================
+# Distribution Config
+# ==============================================================================
+DISTRO_NAME="" # Name for this Downstream of Clear Linux
+DISTRO_URL="" # URL that will be used to host the distro artifacts (update, releases etc)
 
+BUNDLES_REPO="" # Git repository Hosting the bundles definitions
+
+# ==============================================================================
 # Infrastructure config
-DSTREAM_DL_URL="" # URL that will be used to host this Downstream artifacts (update, releases etc)
+# ==============================================================================
+PUBLISHING_HOST="" # The hostname of the server to which release artifacts will be published
+PUBLISHING_ROOT="/srv/pub" # The directory on the server where release artifacts will be published
 
+# Only if using koji:
 KOJI_TAG="" # Koji Tag to be used by this Downstream
 KOJI_URL="" # Kojihub URL
-KOJI_TOPURL="" # Koji Top url (kojifiles)
-BUNDLES_REPO="" # Git repository Hosting the bundles definitions for this Downstream
 
-PUBLISHING_HOST="" # The hostname of the server to which Downstream artifacts will be published
-PUBLISHING_ROOT="/srv/pub" # The directory on the server where Downstream artifacts will be published
-
+# ==============================================================================
 # Workspace config
-BUILD_DIR="/srv/work/${NAMESPACE:-${DSTREAM_NAME}}" # Folder inside the Builder that should be used to run the release process
-STAGING_DIR="/srv/pub/${NAMESPACE:-${DSTREAM_NAME}}" # Folder inside the Builder that should be used to stage the produced artifacts
+# ==============================================================================
+# Folders structure inside the Builder host. Those are assumed to exist and be
+# accessible by the builder user.
+MIXER_ROOT="/srv/mixer"
+MIXER_DIR="${MIXER_ROOT}/${NAMESPACE}"
 
+STAGING_ROOT="/srv/stage"
+STAGING_DIR="${STAGING_ROOT}/${NAMESPACE}"
+
+# ==============================================================================
 # You can export custom environment variables (like proxy) bellow
+# ==============================================================================
